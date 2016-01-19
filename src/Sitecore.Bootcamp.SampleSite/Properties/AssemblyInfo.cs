60a681b06b197cd626de62970357e2f078065b5c
AlenPelin <AlenPelin@sitecore.net>
2016-01-19 16:44:26 +1100

    Implemented sample site

---
+++ src/Sitecore.Bootcamp.SampleSite/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Bootcamp.SampleSite")]
+[assembly: AssemblyProduct("Sitecore.Bootcamp")]
+[assembly: AssemblyCopyright("Copyright © Sitecore 2016")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyInformationalVersion("0.0.2.0")]
