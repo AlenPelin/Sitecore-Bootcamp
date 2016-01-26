5ac9582bb0fe7ea29304e0ed963b29ffabbd022c
AlenPelin <AlenPelin@gmail.com>
2016-01-26 18:00:47 +1100

    Released 0.0.4.0

--- src/Sitecore.Bootcamp.Core/Properties/AssemblyInfo.cs
+++ src/Sitecore.Bootcamp.Core/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("Sitecore.Bootcamp.Core.UnitTests")]
 
-[assembly: AssemblyVersion("0.0.3.3")]
-[assembly: AssemblyFileVersion("0.0.3.3")]
-[assembly: AssemblyInformationalVersion("0.0.3.3")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
+[assembly: AssemblyInformationalVersion("0.0.4.0")]
