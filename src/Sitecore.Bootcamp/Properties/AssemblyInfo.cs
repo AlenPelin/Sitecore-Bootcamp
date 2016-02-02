b5f2724d77acc43dde8320961f6abc5e4902b8a7
AlenPelin <AlenPelin@gmail.com>
2016-02-02 17:52:27 +1100

    Releasing 0.0.5.2

--- src/Sitecore.Bootcamp/Properties/AssemblyInfo.cs
+++ src/Sitecore.Bootcamp/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Sitecore 2016")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.5.1")]
-[assembly: AssemblyFileVersion("0.0.5.1")]
-[assembly: AssemblyInformationalVersion("0.0.5.1")]
+[assembly: AssemblyVersion("0.0.5.2")]
+[assembly: AssemblyFileVersion("0.0.5.2")]
+[assembly: AssemblyInformationalVersion("0.0.5.2")]
