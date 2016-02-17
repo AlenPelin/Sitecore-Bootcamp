a7bd0ea48822050c4d607014f8998a639e8e1c7a
AlenPelin <AlenPelin@gmail.com>
2016-02-17 11:25:44 +1100

    Fixed too long MongoDB name (closes #6)

--- src/Sitecore.Bootcamp.Core/Processors/InstallMongoDatabases.cs
+++ src/Sitecore.Bootcamp.Core/Processors/InstallMongoDatabases.cs
@@ -1,6 +1,7 @@
 ﻿namespace Sitecore.Bootcamp.Core.Processors
 {
   using System.Configuration;
+  using System.IO;
   using Sitecore.Diagnostics.Base;
 
   internal class InstallMongoDatabases : InstallDatabaseProcessorBase
@@ -31,7 +32,11 @@
 
         var addNode = connectionStrings.CreateElement("add");
         addNode.SetAttribute("name", name);
-        addNode.SetAttribute("connectionString", string.Format("mongodb://localhost:27017/{0}_{1}", args.Server.MapPath("/").Replace("\\", "_").Replace(":", "").Replace(".", "_"), name.Replace("\\", "_").Replace(":", "").Replace(".", "_")));
+
+        var db = Path.Combine(args.Server.MapPath("/").TrimEnd('\\'), name);
+        db = db.Replace("\\\\", "\\").Replace("\\", "_").Replace(":", "").Replace(".", "_");
+        db = db.Length > 64 ? db.Substring(db.Length - 64) : db; // mongo db limit is 64 chars for db name
+        addNode.SetAttribute("connectionString", string.Format("mongodb://localhost:27017/{0}", db));
         connectionStrings.DocumentElement.AppendChild(addNode);
         args.AddedConnectionStrings.Add(name);
       }
