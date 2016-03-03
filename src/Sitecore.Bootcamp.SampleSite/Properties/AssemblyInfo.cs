50eaf8e04a94953a111bb42fd6179fa5ca115829
AlenPelin <AlenPelin@gmail.com>
2016-03-03 17:47:37 +1100

    Minor fix

--- src/Sitecore.Bootcamp.SampleSite/Properties/AssemblyInfo.cs
+++ src/Sitecore.Bootcamp.SampleSite/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Sitecore 2016")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.4.0")]
-[assembly: AssemblyFileVersion("0.0.4.0")]
-[assembly: AssemblyInformationalVersion("0.0.4.0")]
+[assembly: AssemblyVersion("0.0.5.7")]
+[assembly: AssemblyFileVersion("0.0.5.7")]
+[assembly: AssemblyInformationalVersion("0.0.5.7")]
